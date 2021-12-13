c11ef61e70295609359fbf04e755eb71ba59690b
Roy Xiang <git@royxiang.me>
2021-12-13 18:39:11 +0800

    build(deps): bump python-mirai-core from 0.9.0.post1 to 0.9.0.post2

--- app/install.sh
+++ app/install.sh
@@ -8,7 +8,7 @@ HOME=$APPDIR
 pip3 install -r "$APPDIR/requirements.txt"
 pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.5.post1#egg=efb-wechat-slave
 pip3 install --no-deps -e git+https://github.com/milkice233/efb-qq-plugin-mirai.git@9fcf207#egg=efb-qq-plugin-mirai
-pip3 install --no-deps -e git+https://github.com/RoyXiang/python-mirai-core.git@v0.9.0.post1#egg=python-mirai-core
+pip3 install --no-deps -e git+https://github.com/RoyXiang/python-mirai-core.git@v0.9.0.post2#egg=python-mirai-core
 
 rm -rf .cache Library
 
