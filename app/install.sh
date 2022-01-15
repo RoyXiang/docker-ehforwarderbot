8c4e10584c8f5f816f2d8b22230a1af9a2929911
Roy Xiang <git@royxiang.me>
2022-01-15 17:05:22 +0800

    build(deps): bump efb-wechat-slave from 2.0.6.post1 to 2.0.6.post2

--- app/install.sh
+++ app/install.sh
@@ -6,7 +6,7 @@ APPDIR=$(pwd -L)
 HOME=$APPDIR
 
 pip3 install -r "$APPDIR/requirements.txt"
-pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.6.post1#egg=efb-wechat-slave
+pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.6.post2#egg=efb-wechat-slave
 pip3 install --no-deps -e git+https://github.com/milkice233/efb-qq-plugin-mirai.git@9fcf207#egg=efb-qq-plugin-mirai
 pip3 install --no-deps -e git+https://github.com/RoyXiang/python-mirai-core.git@v0.9.0.post3#egg=python-mirai-core
 
