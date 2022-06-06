b83ce21fc440e986bddddd1ee48c96a616ab0c3e
Roy Xiang <git@royxiang.me>
2022-06-06 14:04:06 +0800

    build(docker): do not install pilk independently

--- app/install.sh
+++ app/install.sh
@@ -6,7 +6,6 @@ APPDIR=$(pwd -L)
 HOME=$APPDIR
 
 pip3 install -r "$APPDIR/requirements.txt"
-pip3 install pilk
 pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.7.post1#egg=efb-wechat-slave
 pip3 install -e git+https://github.com/RoyXiang/efb-qq-plugin-go-cqhttp.git@49ce724#egg=efb-qq-plugin-go-cqhttp
 
