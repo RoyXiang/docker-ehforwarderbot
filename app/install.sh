16f113284070512c2b1de1f814a06dc5c660a484
Roy Xiang <git@royxiang.me>
2022-07-02 09:17:45 +0800

    build(deps): bump efb-wechat-slave from v2.0.7.post1 to v2.0.7.post2

--- app/install.sh
+++ app/install.sh
@@ -6,8 +6,9 @@ APPDIR=$(pwd -L)
 HOME=$APPDIR
 
 pip3 install -r "$APPDIR/requirements.txt"
-pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.7.post1#egg=efb-wechat-slave
-pip3 install -e git+https://github.com/RoyXiang/efb-qq-plugin-go-cqhttp.git@49ce724#egg=efb-qq-plugin-go-cqhttp
+pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.7.post2#egg=efb-wechat-slave
+pip3 install -e git+https://github.com/ehForwarderBot/efb-qq-slave.git@0df5ddc#egg=efb-qq-slave
+pip3 install -e git+https://github.com/RoyXiang/efb-qq-plugin-go-cqhttp.git@9085d86#egg=efb-qq-plugin-go-cqhttp
 
 rm -rf .cache Library
 
