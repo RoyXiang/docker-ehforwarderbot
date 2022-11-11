de23a3a969ff8e9995418282b6c84a925391a38e
Roy Xiang <git@royxiang.me>
2022-11-11 15:13:53 +0800

    build(deps): bump efb-qq-plugin-go-cqhttp from 9085d86 to 39f628a

--- app/install.sh
+++ app/install.sh
@@ -6,9 +6,15 @@ APPDIR=$(pwd -L)
 HOME=$APPDIR
 
 pip3 install -r "$APPDIR/requirements.txt"
+# install forked version of efb-wechat-slave
 pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.7.post2#egg=efb-wechat-slave
-pip3 install -e git+https://github.com/ehForwarderBot/efb-qq-slave.git@0df5ddc#egg=efb-qq-slave
-pip3 install -e git+https://github.com/RoyXiang/efb-qq-plugin-go-cqhttp.git@9085d86#egg=efb-qq-plugin-go-cqhttp
+# install latest version of efb-qq-slave
+pip3 install --no-deps -e git+https://github.com/ehForwarderBot/efb-qq-slave.git@0df5ddc#egg=efb-qq-slave
+pip3 uninstall -y cqhttp
+# install latest version of efb-qq-plugin-go-cqhttp
+pip3 install "aiocqhttp~=1.4.3"
+pip3 install "pilk~=0.0.2"
+pip3 install --no-deps -e git+https://github.com/ehForwarderBot/efb-qq-plugin-go-cqhttp.git@39f628a#egg=efb-qq-plugin-go-cqhttp
 
 rm -rf .cache Library
 
